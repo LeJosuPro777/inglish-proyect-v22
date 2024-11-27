f54b703c02f2bff52e133325e7f31605e9561aa9
Josue David <pato78478@gmail.com>
2024-11-27 18:19:26 -0500

    ##

--- src/components/RoutesGames.jsx
+++ src/components/RoutesGames.jsx
@@ -38,9 +38,9 @@ export const GameContentRoutes = ({memoryContent}) => {
             <Link className="link" to={"/hangman"}>
               Hangman
             </Link>
-            <Link className="link" to={"/create-new-game"}>hola</Link>
           </div>
         </nav>
+            <Link className="noclass" to={"/create-new-game"}>XDDD</Link>
       </div>
 
       <div className="main-content">
